d0c80bb9f8054a1f0315f38bef31407c576c332f
Mikkel Markvardsen <mikkelmarkvardsen@gmail.com>
2014-01-07 14:44:51 +0100

    Added mobile drawer design

--- js/index.js
+++ js/index.js
@@ -1,5 +1,24 @@
+window.onload = function () {
+
+    var slideMenuButton = document.getElementById('slide-menu-button');
+    slideMenuButton.onclick = function (e) {
+        var cl = document.body.classList;
+        if (cl.contains('left-nav')) {
+            cl.remove('left-nav');
+        } else {
+            cl.add('left-nav');
+        }
+    };
+
+}
+
 function TodoCtrl($scope) {
-  $scope.todos = [];
+  $scope.todos = [
+  {
+	  text:'learn your mom', done:false
+  }
+  
+  ];
  
   $scope.addTodo = function() {
     $scope.todos.push({text:$scope.todoText, done:false});

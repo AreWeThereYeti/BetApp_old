40c9d5abd9d8232d1a56c1777fa4bbccacb78a2d
jonaslading <j_lading@hotmail.com>
2014-02-21 14:16:30 +0100

    removed it again (wasn't needed)

--- src/js/angular/directives/directives.js
+++ src/js/angular/directives/directives.js
@@ -1,9 +1,14 @@
-var betApp = angular.module('betApp', [])	/* CORS cross domain fix */
+var betApp = angular.module('betApp', [])	
+/* -----CORS cross domain fix 
+
 	.config(function($httpProvider) {
 		$httpProvider.defaults.useXDomain = true; 
 		delete $httpProvider.defaults.headers
 			.common['X-Requested-With'];
+		console.log("configging");
+
 	})
+*/
 	.directive('ngApp', function() {
 	    return {
 	    controller:AppCtrl,
